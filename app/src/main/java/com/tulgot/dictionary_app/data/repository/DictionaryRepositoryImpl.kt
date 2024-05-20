46a70fdbb0a67b6d147177e1051d28760259821a
francisco.carrillo <tulgot@gmail.com>
2024-05-20 15:59:52 -0600

    RepositoryModule start

--- app/src/main/java/com/tulgot/dictionary_app/data/repository/DictionaryRepositoryImpl.kt
+++ app/src/main/java/com/tulgot/dictionary_app/data/repository/DictionaryRepositoryImpl.kt
@@ -3,12 +3,14 @@ package com.tulgot.dictionary_app.data.repository
 import android.app.Application
 import com.tulgot.dictionary_app.R
 import com.tulgot.dictionary_app.data.api.DictionaryApi
+import com.tulgot.dictionary_app.data.mapper.toWordItem
 import com.tulgot.dictionary_app.domain.model.WordItem
 import com.tulgot.dictionary_app.domain.repository.DictionaryRepository
 import com.tulgot.dictionary_app.util.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class DictionaryRepositoryImpl @Inject constructor(
@@ -26,8 +28,29 @@ class DictionaryRepositoryImpl @Inject constructor(
             }catch (e: HttpException){
                 e.printStackTrace()
                 emit(Result.Error(application.getString((R.string.can_t_get_result))))
+                emit(Result.Loading(false))
+                return@flow
+            }catch (e: IOException){
+                e.printStackTrace()
+                emit(Result.Error(application.getString((R.string.can_t_get_result))))
+                emit(Result.Loading(false))
+                return@flow
+            }catch (e: Exception){
+                e.printStackTrace()
+                emit(Result.Error(application.getString((R.string.can_t_get_result))))
+                emit(Result.Loading(false))
+                return@flow
             }
 
+            remoteWordResultDto?.let { wordResultDto ->
+                wordResultDto[0]?.let {wordItemDto ->
+                    emit(Result.Success(wordItemDto.toWordItem()))
+                    emit(Result.Loading(false))
+                    return@flow
+                }
+            }
+
+            emit(Result.Error(application.getString(R.string.can_t_get_result)))
             emit(Result.Loading(false))
         }
     }
